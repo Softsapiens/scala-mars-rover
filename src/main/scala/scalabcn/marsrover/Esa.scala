04551c556e915cedfae5eb9ff85eb49f241fb964
Jordi Pradel <jordi.pradel@agilogy.com>
2014-03-10 18:18:21 +0100

    Example simplified

--- src/main/scala/scalabcn/marsrover/Esa.scala
+++ src/main/scala/scalabcn/marsrover/Esa.scala
@@ -2,14 +2,13 @@ package scalabcn.marsrover
 
 import akka.actor.{Terminated, Props, Actor}
 import akka.event.LoggingReceive
-import scalabcn.marsrover.MarsSatellite.{AbortMission, GoToMars}
+import scalabcn.marsrover.MarsSatellite.AbortMission
 
 class Esa extends Actor {
 
   val rover = context.actorOf(Props[MarsRover], "mars-rover")
   val satellite = context.actorOf(Props(classOf[MarsSatellite], rover), "mars-satellite")
   context.watch(satellite)
-  satellite ! GoToMars
   satellite ! AbortMission
 
   override def receive = LoggingReceive {
--- src/main/scala/scalabcn/marsrover/Mars.scala
+++ src/main/scala/scalabcn/marsrover/Mars.scala
@@ -2,17 +2,15 @@ package scalabcn.marsrover
 
 import akka.actor.{ActorRef, Actor}
 import akka.event.LoggingReceive
-import scalabcn.marsrover.MarsSatellite.{AbortMission, GoToMars}
+import scalabcn.marsrover.MarsSatellite.AbortMission
 
 object MarsSatellite{
-case object GoToMars
 case object AbortMission
 }
 
 class MarsSatellite(rover:ActorRef) extends Actor{
 
   override def receive = LoggingReceive {
-    case GoToMars =>
     case AbortMission =>
       rover ! SelfDestruct
       context.stop(self)
